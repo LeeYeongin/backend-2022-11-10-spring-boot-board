252c445ee0dd985010b89a5818f1aab37350eb48
LeeYeongin <youngin5757@gmail.com>
2023-01-09 13:10:37 +0900

    Entity, Repository, Login

--- src/main/java/com/yeongin/board/service/AuthService.java
+++ src/main/java/com/yeongin/board/service/AuthService.java
@@ -1,14 +1,45 @@
 package com.yeongin.board.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yeongin.board.dto.response.ResponseDto;
+import com.yeongin.board.entity.MemberEntity;
+import com.yeongin.board.repository.MemberRepository;
 import com.yeongin.board.dto.auth.AuthPostDto;
 import com.yeongin.board.dto.auth.LoginDto;
 
 // @Service : 해당 클래스가 Service 레이어 역할을 함
 @Service
 public class AuthService {
+	
+	@Autowired MemberRepository memberRepository;
+	
+	public String hello() {
+		// Entity Class로 entity 빌드
+		MemberEntity memberEntity = 
+				MemberEntity
+				.builder()
+				.email("qwe@qwe.com")
+				.password("qwe123")
+				.nickname("jiraynor")
+				.telNumber("010-1234-5678")
+				.address("busan")
+				.build();
+		// 빌드한 Entity를 데이터베이스에 저장
+		memberRepository.save(memberEntity);
+		
+		// MemberRepository가 상속받은 JpaRepository 메서드를 사용하여 데이터 검색
+		MemberEntity savedMemberEntity = memberRepository.findById("qwe@qwe.com").get();
+		
+		// MemberRepository에 작성한 커스텀 메서드를 
+		List<MemberEntity> list = memberRepository.myFindAll("qwe@qwe.com");
+		System.out.println(list.toString());
+		
+		return savedMemberEntity.getNickname();
+	}
 	
 	public ResponseDto<LoginDto> login(AuthPostDto dto){
 		LoginDto result = new LoginDto("JWT", 3600000);
