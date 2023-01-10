3c6bc40cea4529548cd256ddd5849a7402d900d1
LeeYeongin <youngin5757@gmail.com>
2023-01-10 12:35:56 +0900

    login, get user data

--- src/main/java/com/yeongin/board/controller/AuthController.java
+++ src/main/java/com/yeongin/board/controller/AuthController.java
@@ -11,27 +11,28 @@ import com.yeongin.board.dto.auth.AuthPostDto;
 import com.yeongin.board.dto.auth.LoginDto;
 import com.yeongin.board.dto.response.ResponseDto;
 import com.yeongin.board.service.AuthService;
+
 @RestController
 @RequestMapping("api/auth/")
 public class AuthController {
-	
+
 	// @Autowired : 해당하는 클래스 인스턴스를 자동으로 생성(주입) 해줌
-	@Autowired AuthService authService;
-	
+	@Autowired
+	AuthService authService;
+
 	@PostMapping("")
 	public ResponseDto<LoginDto> login(@RequestBody AuthPostDto requestBody) {
 //		LoginDto result = new LoginDto("JWT", 3600000);
 //		return ResponseDto.setSuccess("login success", result);
-		
+
 		return authService.login(requestBody);
 	}
-	
+
 	@GetMapping("")
 	public String hello() {
 		return authService.hello();
 	}
-	
+
 	/* ---------------------------------------------------------------------- */
-	
-	
+
 }
